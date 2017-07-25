1e6044c7716381d7478af70602ee37c560ea7f14
Hassan Tariq <Hassan@Hassans-Macbook.local>
2017-07-26 00:50:31 +0500

    added welcome page

---
+++ QuizApplication/app/src/main/java/quicksolutions/quizapplication/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package quicksolutions.quizapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
